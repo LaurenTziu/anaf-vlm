5a99edb3140ef4864d19b5a84bbc1b26ae1180bd
laurentiuv <laurentiu@managementit.ro>
2023-05-08 13:13:01 +0000

    Bug fixes

--- src/Resources/Concerns/Transportable.php
+++ src/Resources/Concerns/Transportable.php
@@ -9,11 +9,14 @@ use Anaf\ValueObjects\TaxIdentificationNumber;
 
 trait Transportable
 {
+    private Transporter $transporter;
+    private TaxIdentificationNumber $taxIdentificationNumber;
     /**
      * Creates a Client instance with the given Tax Identification Number.
      */
-    public function __construct(private Transporter $transporter, private TaxIdentificationNumber $taxIdentificationNumber)
+    public function __construct( Transporter $transporter,  TaxIdentificationNumber $taxIdentificationNumber)
     {
-        // ..
+        $this->transporter = $transporter;
+        $this->taxIdentificationNumber = $taxIdentificationNumber;
     }
 }
